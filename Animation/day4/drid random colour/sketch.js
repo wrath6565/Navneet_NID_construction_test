6e11075ea674f9c520e1e5cb9424fd727a5f4202
wrath6565 <navneetsinghds@gmail.com>
2025-10-10 01:52:31 +0530

    work

--- Animation/day4/drid random colour/sketch.js
+++ Animation/day4/drid random colour/sketch.js
@@ -1,5 +1,6 @@
-let size;
-size = 100;
+let colour =["#26ff79ff", "#c9fc3cff", "#fff596ff","#cf7979ff"]
+let size = 100
+
 function setup() {
   frameRate(50);
   createCanvas(innerWidth, innerHeight);
@@ -14,7 +15,9 @@ function draw() {
   // create grid squares
   for(let i = 0; i <width; i = i+size){
     for(let j = 0; j <height; j = j+size){
-      fill(255,60,150);
+
+      let choice = floor(random(0,colour.length))
+      fill(colour[choice]);
       
       push();
       translate(i,j);
