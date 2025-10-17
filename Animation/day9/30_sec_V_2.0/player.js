b25cb263290c9cf9895a52b31a60e869875c217c
wrath6565 <navneetsinghds@gmail.com>
2025-10-17 08:58:00 +0530

    lilu

--- Animation/day9/30_sec_V_2.0/player.js
+++ Animation/day9/30_sec_V_2.0/player.js
@@ -3,14 +3,14 @@ class Player {
         this.x = x;
         this.y = y;
         this.xSize =//to be filled
-        this.ySize =//to be filled
-        this.xDir = xDir;
+            this.ySize =//to be filled
+            this.xDir = xDir;
         this.yDir = yDir;
 
     }
 
     show() {
-        image(this.x, this.y, this.xSize, this.ySize,this.xDir,this.yDir)
+        image(this.x, this.y, this.xSize, this.ySize, this.xDir, this.yDir)
     }
 
     // Collision with walls
@@ -27,8 +27,20 @@ class Player {
         }
     }
 
-    move(){
-        
+    move() {
+        if (gamePaused) return; // ignore input if game is paused
+
+        if (keyCode == UP_ARROW) {
+            row = 3; xDir = 0; yDir = -7;
+        } else if (keyCode == DOWN_ARROW) {
+            row = 0; xDir = 0; yDir = 7;
+        } else if (keyCode == LEFT_ARROW) {
+            row = 1; xDir = -7; yDir = 0;
+        } else if (keyCode == RIGHT_ARROW) {
+            row = 2; xDir = 7; yDir = 0;
+        }
     }
+
 }
+
 
